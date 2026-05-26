86007de60b06bf0ea9588f190eb7b69dca1105af
Saurav Rohilla <saurav.rohilla@enpass.io>
2026-05-26 18:34:47 +0530

    Functionality : Search Pokemons added into Header

--- src/components/AbilityCard.tsx
+++ src/components/AbilityCard.tsx
@@ -1,28 +1,47 @@
-import { useGetPokemonAbilitiesQuery } from "../services/apiSlice";
-import SubHeadingH4 from "./SubHeadingH4";
+import { useGetPokemonAbilitiesQuery } from '../services/apiSlice'
+import SubHeadingH4 from './SubHeadingH4'
 
-export default function AbilityCard({ text, abilityId, isHidden }: { text: string, abilityId: number, isHidden: boolean }) {
-    const { data: abilities, isLoading: isLoadingAbilities } = useGetPokemonAbilitiesQuery(abilityId);
-    return (
-        <>
-            {isHidden}
-            <div className="p-5 rounded-[32px] dark:bg-[#202020] bg-[#f3f4f6] text-[#64748B]">
-                {
-                    isHidden ?
-                        <div className="text-[#0F172A] dark:text-[#0F172A] font-bold items-start flex justify-between">
-                            <SubHeadingH4 name={text} fontSize="16px" />
-                            <span class={"border-full text-[#475569] text-[12px] bg-[#e2e8f0] py-1 px-2 rounded-[32px]"}>Hidden</span>
-                        </div>
-                        :
-                        <div className="text-[#BC0100] dark:!text-[#BC0100]! font-bold">
-                            <SubHeadingH4 name={text} fontSize="16px" />
-                        </div>
-                }
-                {
-                    isLoadingAbilities ? "" :
-                        <p>{abilities?.flavor_text_entries.find((item) => item.language.name === "en")?.flavor_text}</p>
-                }
-            </div>
-        </>
-    )
+export default function AbilityCard({
+  text,
+  abilityId,
+  isHidden,
+}: {
+  text: string
+  abilityId: number
+  isHidden: boolean
+}) {
+  const { data: abilities, isLoading: isLoadingAbilities } = useGetPokemonAbilitiesQuery(abilityId)
+  return (
+    <>
+      {isHidden}
+      <div className="rounded-[32px] bg-[#f3f4f6] p-5 text-[#64748B] dark:bg-[#202020]">
+        {isHidden ? (
+          <div className="flex items-start justify-between font-bold text-[#0F172A] dark:text-[#0F172A]">
+            <SubHeadingH4 name={text} fontSize="16px" />
+            <span
+              class={
+                'border-full min-w-16 rounded-[32px] bg-[#e2e8f0] px-2 py-1 text-[12px] text-[#475569]'
+              }
+            >
+              Hidden
+            </span>
+          </div>
+        ) : (
+          <div className="dark:!text-[#BC0100]! font-bold text-[#BC0100]">
+            <SubHeadingH4 name={text} fontSize="16px" />
+          </div>
+        )}
+        {isLoadingAbilities ? (
+          ''
+        ) : (
+          <p>
+            {
+              abilities?.flavor_text_entries.find((item) => item.language.name === 'en')
+                ?.flavor_text
+            }
+          </p>
+        )}
+      </div>
+    </>
+  )
 }
--- src/components/HeaderSearchBar.tsx
+++ src/components/HeaderSearchBar.tsx
@@ -1,13 +1,56 @@
-import searchIcon from "../assets/images/Container.svg"
+import { useEffect, useState } from 'preact/hooks'
+import searchIcon from '../assets/images/Container.svg'
+import { handleInput } from '../utlities/helper'
+import type { PokemonListItem, PokemonListResponse } from '../types/searchPokemons'
+
 export default function HeaderSearchBar() {
-    return (
-        <>
-            <div className="w-1/4">
-                <div className="py-4 px-2 bg-[#F3F4F5] flex items-center justify-around rounded-[40px]">
-                    <img src={searchIcon} alt="Search Icon" />
-                    <input type="text" id='search-pokemon' class={"outline-none border-none bg-transparent"} placeholder={"Search Pokemon..."} />
-                </div>
-            </div>
-        </>
-    )
+  const [value, setValue] = useState<string>('')
+  const [data, setData] = useState<PokemonListResponse>()
+  let items = null
+  if (data) {
+    items = data.results.filter((item) => item.name.toLowerCase().includes(value.toLowerCase()))
+  }
+
+  useEffect(() => {
+    async function fetchPokemon() {
+      const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=100000')
+      setData(await res.json())
+    }
+    fetchPokemon()
+  }, [value])
+
+  return (
+    <>
+      <div className="relative w-1/4">
+        <div className="flex items-center justify-around gap-2 rounded-[40px] bg-[#F3F4F5] px-2 py-4">
+          <img src={searchIcon} alt="Search Icon" />
+          <input
+            type="text"
+            onKeyPress={(e) => handleInput(e, setValue)}
+            onInput={(e) => setValue((e.target as HTMLInputElement).value)}
+            value={value}
+            id="search-pokemon"
+            class={'border-none bg-transparent outline-none'}
+            placeholder={'Search Pokemon...'}
+          />
+        </div>
+        {items && items.length > 0 && value !== '' && (
+          <ul className={'absolute max-h-30 w-full overflow-scroll bg-white p-2'} id="pokemon-list">
+            {items.map((item: PokemonListItem) => {
+              console.log('Itmes: ', { items })
+
+              console.log({ value })
+              return (
+                <li onClick={() => setValue('')}>
+                  <a href={item.url.slice(0, -1).split('/').at(-1)} className={'capitalize'}>
+                    {item.name.split('-').join(' ')}
+                  </a>
+                </li>
+              )
+            })}
+          </ul>
+        )}
+      </div>
+    </>
+  )
 }
